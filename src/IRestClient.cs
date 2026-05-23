006003ddc059be978344fdd459ab065addda4873
mtfields <mfields@clcohio.org>
2026-05-22 23:51:58 -0400

    Add ExecuteAsync overloads to IRestClient

--- src/IRestClient.cs
+++ src/IRestClient.cs
@@ -1,13 +1,18 @@
 ﻿using Clc.Rest.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Clc.Rest
 {
     public interface IRestClient
     {
+        Task<IRestResponse<T>> ExecuteAsync<T>(RestRequest request, CancellationToken cancellationToken = default);
+
+        Task<IRestResponse<T>> ExecuteAsync<T>(string url, CancellationToken cancellationToken = default);
+
+        Task<IRestResponse<T>> ExecuteAsync<T>(HttpMethod method, string url, CancellationToken cancellationToken = default);
+
         IRestResponse<T> Execute<T>(RestRequest request);
     }
 }
