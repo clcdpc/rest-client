3b8eb4de88dc9807caa7b3b805ab081fabe12670
mtfields <mfields@clcohio.org>
2026-05-27 21:51:41 -0400

    Add focused JsonNetDeserializer unit tests (#113)

--- tests/Clc.Rest.Client.Tests/Serialization/JsonNetDeserializerTests.cs
+++ tests/Clc.Rest.Client.Tests/Serialization/JsonNetDeserializerTests.cs
@@ -1,6 +1,7 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Clc.Rest.Serialization;
 using System.Collections.Generic;
+using Clc.Rest.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 
 namespace Clc.Rest.Client.Tests.Serialization;
 
@@ -10,6 +11,9 @@ public class JsonNetDeserializerTests
     private class TestModel
     {
         public int Id { get; set; }
+
+        public bool IsEnabled { get; set; }
+
         public string? Name { get; set; }
     }
 
@@ -18,7 +22,7 @@ public class JsonNetDeserializerTests
     {
         // Arrange
         var deserializer = new JsonNetDeserializer();
-        string json = "{\"Id\": 1, \"Name\": \"Test\"}";
+        var json = "{\"Id\":1,\"IsEnabled\":true,\"Name\":\"Test\"}";
 
         // Act
         var result = deserializer.Deserialize<TestModel>(json);
@@ -26,23 +30,46 @@ public class JsonNetDeserializerTests
         // Assert
         Assert.IsNotNull(result);
         Assert.AreEqual(1, result.Id);
+        Assert.IsTrue(result.IsEnabled);
         Assert.AreEqual("Test", result.Name);
     }
 
     [TestMethod]
-    public void Deserialize_EmptyJson_ReturnsEmptyObject()
+    public void Deserialize_EmptyObjectJson_ReturnsNewInstanceWithDefaultValues()
     {
         // Arrange
         var deserializer = new JsonNetDeserializer();
-        string json = "{}";
+        var json = "{}";
 
         // Act
         var result = deserializer.Deserialize<TestModel>(json);
 
         // Assert
         Assert.IsNotNull(result);
-        Assert.AreEqual(0, result.Id);
+        Assert.AreEqual(default, result.Id);
+        Assert.AreEqual(default, result.IsEnabled);
         Assert.IsNull(result.Name);
+    }
+
+    [TestMethod]
+    public void Deserialize_JsonArray_ReturnsList()
+    {
+        // Arrange
+        var deserializer = new JsonNetDeserializer();
+        var json = "[{\"Id\":1,\"IsEnabled\":true,\"Name\":\"One\"},{\"Id\":2,\"IsEnabled\":false,\"Name\":\"Two\"}]";
+
+        // Act
+        var result = deserializer.Deserialize<List<TestModel>>(json);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(2, result.Count);
+        Assert.AreEqual(1, result[0].Id);
+        Assert.IsTrue(result[0].IsEnabled);
+        Assert.AreEqual("One", result[0].Name);
+        Assert.AreEqual(2, result[1].Id);
+        Assert.IsFalse(result[1].IsEnabled);
+        Assert.AreEqual("Two", result[1].Name);
     }
 
     [TestMethod]
@@ -52,12 +79,12 @@ public class JsonNetDeserializerTests
         var deserializer = new JsonNetDeserializer();
 
         // Act
-        System.ArgumentNullException? exception = null;
+        ArgumentNullException? exception = null;
         try
         {
             deserializer.Deserialize<TestModel>(null!);
         }
-        catch (System.ArgumentNullException ex)
+        catch (ArgumentNullException ex)
         {
             exception = ex;
         }
@@ -67,21 +94,37 @@ public class JsonNetDeserializerTests
     }
 
     [TestMethod]
-    public void Deserialize_JsonArray_ReturnsList()
+    public void Deserialize_EmptyString_ReturnsNull()
     {
         // Arrange
         var deserializer = new JsonNetDeserializer();
-        string json = "[{\"Id\": 1, \"Name\": \"One\"}, {\"Id\": 2, \"Name\": \"Two\"}]";
 
         // Act
-        var result = deserializer.Deserialize<List<TestModel>>(json);
+        var result = deserializer.Deserialize<TestModel>(string.Empty);
 
         // Assert
-        Assert.IsNotNull(result);
-        Assert.AreEqual(2, result.Count);
-        Assert.AreEqual(1, result[0].Id);
-        Assert.AreEqual("One", result[0].Name);
-        Assert.AreEqual(2, result[1].Id);
-        Assert.AreEqual("Two", result[1].Name);
+        Assert.IsNull(result);
+    }
+
+    [TestMethod]
+    public void Deserialize_MalformedJson_ThrowsJsonException()
+    {
+        // Arrange
+        var deserializer = new JsonNetDeserializer();
+        var malformedJson = "{\"Id\":1,\"Name\":";
+
+        // Act
+        JsonException? exception = null;
+        try
+        {
+            deserializer.Deserialize<TestModel>(malformedJson);
+        }
+        catch (JsonException ex)
+        {
+            exception = ex;
+        }
+
+        // Assert
+        Assert.IsNotNull(exception);
     }
 }
